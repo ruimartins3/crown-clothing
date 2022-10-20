f3cd7e97903be73774c380c3946e82e0c9de77fa
Rui Martins <ruimartins.profissional@gmail.com>
2022-10-21 00:12:58 +0100

    Crown Clothing

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,54 @@
+import { Fragment, useContext } from "react";
+import { Outlet } from "react-router-dom";
+
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+
+import { CartContext } from "../../contexts/cart.context";
+import { UserContext } from "../../contexts/user.context";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+
+import {
+  LogoContainer,
+  NavigationContainer,
+  NavLink,
+  NavLinks,
+} from "./navigation.styles";
+
+const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen, closeCart } = useContext(CartContext);
+  const closeTheCart = () => closeCart();
+
+  return (
+    <Fragment>
+      <NavigationContainer>
+        <LogoContainer to="/">
+          <CrwnLogo className="logo" />
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/shop">SHOP</NavLink>
+
+          {currentUser ? (
+            <NavLink as="span" onClick={signOutUser}>
+              SIGN OUT
+            </NavLink>
+          ) : (
+            <NavLink to="/auth">SIGN IN</NavLink>
+          )}
+          <CartIcon />
+        </NavLinks>
+        {isCartOpen && (
+          <div onMouseLeave={() => closeTheCart()}>
+            <CartDropdown />
+          </div>
+        )}
+      </NavigationContainer>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
